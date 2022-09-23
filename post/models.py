c5bb5a4077ef6492bc043488dae06c5ab78339b2
khw7876 <khw7876@gmail.com>
2022-09-23 21:13:50 +0900

    feat/ post model 생성

--- post/models.py
+++ post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from user.models import User
 # Create your models here.
+class Post(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField("제목", max_length=50)
+    content = models.CharField("내용", max_length=500)
+    hashtags = models.CharField("해쉬태그", max_length=100)
+    is_active = models.BooleanField("활성화", default=True)
+    views = models.PositiveSmallIntegerField("조회수", default=0)
+    create_date = models.DateTimeField("작성일", auto_now_add=True)
+    update_date = models.DateTimeField("수저일", auto_now=True)
+
+    def __str__(self):
+        return self.name
