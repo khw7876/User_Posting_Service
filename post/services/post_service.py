9289633abd14fb125be6f859824f020bed3409dc
khw7876 <khw7876@gmail.com>
2022-09-25 23:19:48 +0900

    feat/ 사용자가 작성자와 일치하는지 체크하는 로직 구현

--- post/services/post_service.py
+++ post/services/post_service.py
@@ -87,5 +87,19 @@ def delete_post(post_id : int)-> None:
     delete_post_obj.is_active = False
     delete_post_obj.save()
     
+def is_author(user : UserModel, post_id : int)-> bool:
+    """
+    로그인 한 유저가 해당 게시글의 작성자인지를 판단하는 함수
 
+    Args:
+        user (UserModel): "현재 로그인이 되어있는 User",
+        post_id (int) : "권한을 체크할 게시글 id"
+
+    Returns:
+        bool: _description_
+    """
+    check_post_obj = PostModel.objects.get(id = post_id)
+    if check_post_obj.user == user:
+        return True
+    return False
 
