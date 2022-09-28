cc09f01fda1af9b747b470e64e9e16ae048711ce
khw7876 <khw7876@gmail.com>
2022-09-28 23:16:30 +0900

    refac/ user docstring 수정

--- user/services/user_service.py
+++ user/services/user_service.py
@@ -5,6 +5,7 @@ def create_user(create_data : dict[str,str]) -> None:
     Args:
         create_data (dict[str,str]): views.py에서 넘겨준 request.data{
             "username" (str): user의 username,
+            "email" (str): user의 email,
             "password: (str): user의 password
         }
     """
