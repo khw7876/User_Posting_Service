59f2301953e7d60ae21a7ee047ec7989ef755ab0
khw7876 <khw7876@gmail.com>
2022-10-01 12:42:17 +0900

    test/ 비어있는 해시태그에 대한 검증

--- post/tests/services/test_post_service.py
+++ post/tests/services/test_post_service.py
@@ -21,6 +21,7 @@ class TestPostService(TestCase):
         """
         "#"과 ","이 들어있는 해시태그를 사용할 수 있는 형태로 변환해주는 함수에 대한 검증
         case : 성공적으로 분리가 되었을 경우
+        result : 리스트형태로 태그들의 id 저장
         """
         request_data = {"hashtags" : "#태그1,#태그2"}
         hash_tag_data = {}
@@ -34,3 +35,14 @@ class TestPostService(TestCase):
 
         self.assertEqual(hash_tag_data, get_hashtags_list(request_data))
 
+    def test_when_None_tags_geted_in_get_hashtags_list(self) -> None:
+        """
+        "#"과 ","이 들어있는 해시태그를 사용할 수 있는 형태로 변환해주는 함수에 대한 검증
+        case : 비어있는 해시태그를 넣어주었을 경우
+        result : 비어있는 상태로 실행
+        """
+        request_data = {"hashtags" : ""}
+        hash_tag_data = {}
+        hash_tag_data_list = request_data["hashtags"].replace(",", "").split("#")
+        hash_tag_data["hashtags"] = []
+        self.assertEqual(hash_tag_data, get_hashtags_list(request_data))
